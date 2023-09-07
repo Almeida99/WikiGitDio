60f3863e6cbc7b9945ce3f4271f2203635aa3afc
Almeida99 <97049192+Almeida99@users.noreply.github.com>
2023-09-07 10:43:43 -0300

    Add files via upload

---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 70%;
+
+    h3 {
+        font-size: 32px;
+        color: #FAFA;
+    }
+
+    p {
+        font-size:16px;
+        color: #FAFA;
+        margin-bottom:20px;
+    }
+
+    a.remover {
+        color: #FF0000;
+        margin-top:20px;
+    }
+
+    hr {
+        color: #FAFA;
+        margin: 20px 0;
+    }
+`
